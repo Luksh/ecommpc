011c427d13e4db380812f6f9b884cf1a93dd0c3f
Luksh <hotmale_luksh@hotmail.com>
2022-04-17 13:06:57 +0545

    second commit

--- home/models.py
+++ home/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length= 400)
+    image = models.ImageField(upload_to = 'media')
+    slug = models.CharField(max_length= 500, unique= True)
+
+    def __str__(self):
+        return self.name
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length= 400)
+    category = models.ForeignKey(Category, on_delete= models.CASCADE)
+    image = models.ImageField(upload_to = 'media')
+    slug = models.CharField(max_length= 500, unique= True)
+
+    def __str__(self):
+        return self.name
+
+class Slider(models.Model):
+    name = models.CharField(max_length= 400)
+    image = models.ImageField(upload_to = 'media')
+    title = models.TextField()
+    description = models.TextField(blank= True)
+
+    def __str__(self):
+        return self.name
+
+class Contact(models.Model):
+    name = models.CharField(max_length= 400)
+    email = models.EmailField(max_length= 300, blank= True)
+    phone = models.CharField(blank= True, max_length= 200)
+    subject = models.TextField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
