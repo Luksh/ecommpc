52eff1cdd004e73f805f4f699fe317070a36d5d7
Luksh <hotmale_luksh@hotmail.com>
2022-04-25 11:01:51 +0545

    tenth commit

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', HomeView.as_view(), name = 'home'),
-    path('detail/<slug>', ProductDetailView.as_view(), name= 'detail')
+    path('detail/<slug>', ProductDetailView.as_view(), name= 'detail'),
+    path('category/<slug>', CategoryView.as_view(), name= 'category'),
+    path('subcategory/<slug>', SubCategoryView.as_view(), name= 'subcategory')
 ]
